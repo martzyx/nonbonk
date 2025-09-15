db770c37e2a4855f4d2f0e08f4c3479790634c6c
martzyx <martz@nonbonk.com>
2025-09-15 22:23:57 +0200

    Refactor index.html and App.vue for consistency; add deploy workflow

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,30 +1,22 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="hero">
+    <h1 class="headline">this is a sandbox, you're probably in the wrong place</h1>
+    <p>it's online tho!</p>
+    <p>bonkers</p>
+    <p><em>come again</em></p>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.hero {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.headline {
+  font-size: clamp(2rem, 5vw, 3rem);
+  line-height: 1.15;
+  margin: 0;
 }
 </style>
